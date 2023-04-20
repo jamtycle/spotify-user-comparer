0b7a6efa1249cd8b6b014dc65c8561a5a73a2cea
Bruno Ramirez <bramirez206@hotmail.com>
2023-04-19 23:12:56 -0400

    redirect url

--- src/libs/configs.js
+++ src/libs/configs.js
@@ -1,6 +1,6 @@
 const auth_endpoint = "https://accounts.spotify.com/authorize";
 const client_id = "9ed9ef5611c14d1abdb923719f4dd9b9";
-const redirect_uri = "http://localhost:5173";
+const redirect_uri = "https://jamtycle.github.io/spotify-user-comparer/";
 const scopes = [
     "user-library-read",
     "user-top-read",
